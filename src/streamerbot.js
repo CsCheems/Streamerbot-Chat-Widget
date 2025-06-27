2373852b3910052bf6aa83a77710e9605577478a
Jose Manuel Martinez Martinez <jmanuelm94@outlook.com>
2025-06-26 19:49:59 -0600

    se agregaron eventos de sb

--- src/streamerbot.js
+++ src/streamerbot.js
@@ -15,7 +15,56 @@ export function conectarWs() {
 		setTimeout(conectarWs, 5000);
 	};
 
-	ws.onconnect = () => {
-		
+	ws.onopen = () => {
+		setConnectionStatus(true);
+		const subsPayload = {
+			request: "Subscribe",
+			id: "subscribe-all-events",
+			events:{
+				twitch: [
+					"ChatMessage",
+					"RewardRedemption",
+				]
+			}
+		};
+	ws.send(JSON.stringify(subsPayload));
+	};
+
+	ws.onmessage = (event) => {
+		const wsdata = JSON.parse(event.data);
+		if(!wsdata?.event?.type) return;
+
+		const {source, type} = wsdata.event;
+
+		const data = wsdata.data;
+
+		switch (source) {
+			case 'Twitch':
+				switch (type){
+					case 'ChatMessage': ChatMessage; break;
+					case 'RewardRedemption': RewardRedemption; break;
+					case 'UserTimeOut': TimeoutUser; break;
+					case 'UserBanned': BannedUser; break; 
+				}
+		}
 	}
+}
+
+//ESTADO DE CONEXION A STREAMERBOT
+function setConnectionStatus(connected){
+    let statusContainer = document.getElementById('status-container');
+    if(connected){
+        statusContainer.style.background = "#2FB774";
+        statusContainer.innerText = "CONECTADO!";
+        statusContainer.style.opacity = 1;
+        setTimeout(() => {
+            statusContainer.style.transition = "all 2s ease";
+            statusContainer.style.opacity = 0;
+        }, 1000);
+    }else{
+        statusContainer.style.background = "FF0000";
+        statusContainer.innerText = "CONECTANDO...";
+        statusContainer.style.transition = "";
+        statusContainer.style.opacity = 1;
+    }
 }
