df78aaf630f02c82f8f3ee4d02645c71ae6cb8f5
CsCheems <j.Dev.mx@outlook.com>
2025-06-27 21:14:12 -0600

    toques finales

--- script.js
+++ script.js
@@ -314,8 +314,7 @@ async function RecompensaChat(data) {
 	const siguienteMensaje = document.createElement("li");
 	siguienteMensaje.id = rewardId;
 	siguienteMensaje.dataset.uid = uid;
-    siguienteMensaje.style.marginTop= "5xp";
-    siguienteMensaje.style.marginBottom= "5xp";
+    siguienteMensaje.style.marginTop = "5px";
 
 	// Mover contenido de tempDiv al li
 	while (tempDiv.firstChild) {
